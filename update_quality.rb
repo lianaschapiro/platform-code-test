8b419d1e25d9f6307820bfe514646fc4dc20c655
lianaschapiro <liana.schapiro@gmail.com>
2019-03-14 12:46:13 -0400

    refactor blue distinction plus code

--- update_quality.rb
+++ update_quality.rb
@@ -1,47 +1,49 @@
-require 'award'
+require './award.rb'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
+    # Blue Distinction Plus values never change
+    if award.name != 'Blue Distinction Plus'
+      # All other awards decrease in quality except Blue First and Blue Compare
+      if award.name != 'Blue First' && award.name != 'Blue Compare'
+        if award.quality > 0
           award.quality -= 1
         end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
       else
+        # Blue First and Blue Compare increase in quality
         if award.quality < 50
           award.quality += 1
+          if award.name == 'Blue Compare'
+            if award.expires_in < 11
+              if award.quality < 50
+                award.quality += 1
+              end
+            end
+            if award.expires_in < 6
+              if award.quality < 50
+                award.quality += 1
+              end
+            end
+          end
+        end
+      end
+      award.expires_in -= 1
+      if award.expires_in < 0
+        if award.name != 'Blue First'
+          if award.name != 'Blue Compare'
+            if award.quality > 0
+              # after expiration, awards decrease yet another point (except Blue First and Blue Compare)
+              award.quality -= 1
+            end
+          # Blue Compare quality drops to 0 after expiration
+          else
+            award.quality = 0
+          end
+        # Blue First increases extra quality point after expiration
+        else
+          if award.quality < 50
+            award.quality += 1
+          end
         end
       end
     end
